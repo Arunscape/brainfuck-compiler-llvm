e7639690985b0674e137e19b0144ec04c714429d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-08-29 15:07:33 -0600

    save before I try formatting this with a new plugin

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 #![warn(clippy::all)]
 
 use clap::Arg;
-use inkwell::builder::Builder;
-use inkwell::context::Context;
-use inkwell::execution_engine::{ExecutionEngine, JitFunction};
-use inkwell::module::Module;
-use inkwell::targets::{InitializationConfig, Target};
-use inkwell::OptimizationLevel;
-use std::error::Error;
-use std::fs;
+use inkwell::{OptimizationLevel, builder::Builder, context::Context, execution_engine::{ExecutionEngine, JitFunction}, module::{Linkage, Module}, targets::{InitializationConfig, Target}};
+use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = clap::App::new("ABC - Arun's Brainfuck Compiler")
@@ -22,6 +16,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .value_name("OUTPUT_FILE")
                 .takes_value(true),
         )
+        .arg(Arg::with_name("asm")
+             .help("Output in human readable assembly"))
         .get_matches();
 
     let output = matches.value_of("output");
@@ -30,5 +26,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = matches.value_of("INPUT").unwrap();
     let input = fs::read_to_string(input)?;
     println!("{}", input);
+
+    let context = Context::create();
+    let module = context.create_module("arun_brainfuck_compiler");
+    let builder = context.create_builder();
+
+    let i64_type = context.i64_type();
+    let main_fn_type = i64_type.fn_type(&[], false);
+    let main_fn = module.add_function("main", main_fn_type, Some(Linkage::External));
+
+    let basic_block = context.append_basic_block(main_fn, "entry");
+    builder.position_at_end(basic_block);
+
+
+
+    builder.build_return(Some(&i64_type.const_zero()));
+    Target::initialize_all(&InitializationConfig::default());
+
+
+
+
     Ok(())
 }
+
+// thanks to Ben Konz for this guide
+// https://benkonz.github.io/building-a-brainfuck-compiler-with-rust-and-llvm/
