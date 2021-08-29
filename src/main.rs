9f41204f9fe0c0c1c33c5d24c8ba50213396ca86
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-08-29 16:32:40 -0600

    some progress, in theory

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use inkwell::{
     context::Context,
     execution_engine::{ExecutionEngine, JitFunction},
     module::{Linkage, Module},
-    targets::{InitializationConfig, Target},
+    targets::{CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine},
     OptimizationLevel,
 };
 use std::{error::Error, fs};
@@ -21,12 +21,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             Arg::with_name("output")
                 .short("o")
                 .value_name("OUTPUT_FILE")
-                .takes_value(true),
+                .takes_value(true)
+                .multiple(false)
+                .number_of_values(1),
         )
         .arg(Arg::with_name("asm").help("Output in human readable assembly"))
+        //.arg(
+        //    Arg::with_name("target_triple")
+        //        .short("tt")
+        //        .value_name("TARGET_TRIPLE")
+        //        .takes_value(true)
+        //        .multiple(false)
+        //        .number_of_values(1),
+        //)
         .get_matches();
-
-    let output = matches.value_of("output");
 
     // required argument, so I can unwrap
     let input = matches.value_of("INPUT").unwrap();
@@ -46,6 +54,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     builder.build_return(Some(&i64_type.const_zero()));
     Target::initialize_all(&InitializationConfig::default());
+
+    //    let target_triple = if let Some(tt) = matches.values_of("target_triple") {
+    //        TargetTriple::create(tt.clone().nth(0).unwrap())
+    //    } else {
+    //        TargetMachine::get_default_triple()
+    //    };
+    //
+
+    let target_triple = TargetMachine::get_default_triple();
+    let cpu = TargetMachine::get_host_cpu_name();
+    let features = TargetMachine::get_host_cpu_features();
+
+    let target = Target::from_triple(&target_triple)?;
+
+    let target_machine = target
+        .create_target_machine(
+            &target_triple,
+            &cpu.to_string(),
+            &features.to_string(),
+            OptimizationLevel::Default,
+            RelocMode::Default,
+            CodeModel::Default,
+        )
+        .ok_or_else(|| {
+            format!(
+                "Could not create target machine\ntarget_triple: {}\ncpu: {}\nfeatures: {}",
+                target_triple, cpu, features
+            )
+        })?;
+
+    if let Some(o) = matches.value_of("output") {
+        target_machine.write_to_file(&module, FileType::Object, o.as_ref())?
+    } else {
+        let m = context.create_module("output");
+        let buffer = target_machine.write_to_memory_buffer(&module, FileType::Assembly)?;
+        println!("{:?}", buffer);
+    };
 
     Ok(())
 }
